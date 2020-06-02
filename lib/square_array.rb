ae16f6c7b6b55df6f550fffb01c15031a7845e8b
Chris Allen <chrisallen415@gmail.com>
2020-06-02 20:19:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,7 @@
 def square_array(array)
   squared = []
-  while squared.length < array.lenght { |element| squared << element ** 2 }
+  while squared.length < array.lenght do |element|
+    squared << element ** 2 
+  end
   squared
 end
