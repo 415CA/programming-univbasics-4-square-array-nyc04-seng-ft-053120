4b5cbd9257b20e6ed5c6d07061737b13346919f4
Chris Allen <chrisallen415@gmail.com>
2020-06-02 20:11:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ new_array = Array.new
 
 def square_array(array)
   while array.length.times do |index|
-    new_array.push(array[index] ** array[index])
+    new_array[index] = (array[index] ** array[index])
   end
 new_array
 end
